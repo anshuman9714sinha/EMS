1c3d0ae621792ea74e01746af56f5658ec79697d
anshuman9714sinha <sinha.anshuman97@gmail.com>
2024-06-09 13:56:50 +0200

    adding environments

--- client/src/app/employee.service.ts
+++ client/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from './employee';
+import { environment } from './environments/environment'
 
 @Injectable({
   providedIn: 'root'
---
+++ client/src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://meanstacklecture-7.onrender.com'
+  };
